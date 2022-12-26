5c24b87f6261644cff6f15298f74ae9623ca12dc
YYSU <jim123820@gmail.com>
2022-12-26 11:32:43 +0800

    add blog production

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+from django.contrib.auth import get_user_model
+
+from .models import Post
+from .serializers import PostSerializer, UserSerializer
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
